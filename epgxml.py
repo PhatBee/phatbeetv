4c1a8b22be4416ebcd8407fd9d209509e4bdb830
PhatBee <157566580+PhatBee@users.noreply.github.com>
2025-08-21 13:36:30 +0700

    Update epgxml.py

--- epgxml.py
+++ epgxml.py
@@ -5,7 +5,7 @@ import gzip
 
 # Danh sách URL EPG của bạn
 urls = [
-    'https://lichphatsong.xyz/schedule/epg.xml.gz',
+   # 'https://lichphatsong.xyz/schedule/epg.xml.gz',
     'https://vnepg.site/epg.xml',
    # 'https://lichphatsong.xyz/schedule/detail?id=dmax',
     'https://epg.pw/api/epg.xml?lang=en&channel_id=369848',
@@ -17,7 +17,8 @@ urls = [
     'https://epg.pw/api/epg.xml?lang=en&channel_id=12453',
     'https://epg.pw/api/epg.xml?lang=en&channel_id=212145',
     'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=430092',
-    'https://epg.pw/api/epg.xml?lang=en&channel_id=381881'
+    'https://epg.pw/api/epg.xml?lang=en&channel_id=381881',
+    'https://epg.pw/api/epg.xml?lang=en&channel_id=403788'
 ]
 
 # Tạo root element <tv>
