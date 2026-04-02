10153b9edb2ee77097e8b221ed8c5b3afd542827
PhatBee <157566580+PhatBee@users.noreply.github.com>
2026-04-02 22:29:18 +0700

    Update epgxml.py

--- epgxml.py
+++ epgxml.py
@@ -12,6 +12,7 @@ urls = [
     # 'http://onnaonlinetv.xyz/epg.xml',
    'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=521261', #Canal+
    'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=521259', #Canal+ Box Office
+   'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=521277', #Canal+ Grand Écran
    'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=400477', #TNT Sports 1
    'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=400480', #TNT Sports 2
    'https://epg.pw/api/epg.xml?lang=en&timezone=QXNpYS9Ib19DaGlfTWluaA%3D%3D&channel_id=400479', #TNT Sports 3
